0e1a92220707c6959d9213c8f5907a4bdfa55612
Adam Breaux <breauxaj@gmail.com>
2014-07-18 10:09:04 -0400

    added docs

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,31 @@
+# Class: dhcp
+#
+# This class installs the dhcp server
+#
+# Parameters:
+#
+#  ensure: (default latest)
+#    Determine the state of the packages
+#
+# Actions:
+#   - Installs the dhcp package
+#
+# Requires:
+#
+# Sample Usage:
+#
+#  For a standard installation, use:
+#
+#    class { 'dhcp':
+#      ensure => 'latest'
+#    }
+#
+#  To remove the installation, use:
+#
+#    class { 'dhcp':
+#      ensure => 'absent'
+#    }
+#
 class dhcp (
   $ensure = 'latest'
 ){
