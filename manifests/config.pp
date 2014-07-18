4d5e8e16eddde2b70cc445ec6fd9f698e52b37a9
Adam Breaux <breauxaj@gmail.com>
2014-07-18 10:46:39 -0400

    added docs

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,3 +1,57 @@
+# Define: dhcp::config
+#
+# This define configures the dhcp server
+#
+# Parameters:
+#
+#  subnet:
+#    Define the subnet for the DHCP server
+#
+#  netmask:
+#    Netmask that corresponds to the subnet
+#
+#  router:
+#    Router for the subnet
+#
+#  domain:
+#    Domain for the subnet
+#
+#  name_servers: (array)
+#    One or more name servers for the subnet
+#
+#  range_start:
+#    Subnet range start
+#
+#  range_end:
+#    Subnet range end
+#
+#  lease_default:
+#    Default lease time
+#
+#  lease_max:
+#    Max lease time
+#
+# Actions:
+#   - Configures the dhcp server
+#
+# Requires:
+#
+# Sample Usage:
+#
+#  For a standard installation, use:
+#
+#    dhcp::config { 'default':
+#      subnet        => '192.168.1.0',
+#      netmask       => '255.255.255.0',
+#      router        => '192.168.1.1',
+#      domain        => 'example.com',
+#      name_servers  => [ '208.67.222.222', '208.67.220.220' ],
+#      range_start   => '192.168.1.32',
+#      range_end     => '192.168.1.47',
+#      lease_default => 21600,
+#      lease_max     => 43200,
+#    }
+#
 define dhcp::config (
   $subnet,
   $netmask,
